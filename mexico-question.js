3f5bbf618d5e393e6dae675630edd7aa9bda7bc0
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 00:26:25 -0600

    added questions to North America and Mexico pages

---
+++ mexico-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("Are insects are part of Mexican Cuisine, an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Determinism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
---
+++ northamerica-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("Is the bisons essential to Native Americans, an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Determinism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
