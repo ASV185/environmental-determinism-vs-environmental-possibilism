1bf0bf0c81516ed4b1cb8d8333cd933a3df6c779
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 00:05:26 -0600

    added question to India page

---
+++ india-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("Is skin color in India an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Determinism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
