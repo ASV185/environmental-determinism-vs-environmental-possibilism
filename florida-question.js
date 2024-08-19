bb3d285d0a6bc6c1013980ad0bc9b142eab28590
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 00:47:18 -0600

    added question to Florida page

---
+++ florida-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("Is Florida creating hurricane resistant homes, an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Possibilism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
