422cfbcd1a67b7536319b92712eb3ab0a39ff41b
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 00:39:12 -0600

    added question to Saudi Arabia page

---
+++ saudiarabia-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("Is the cool method use in Saudi Arabia, an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Possibilism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
