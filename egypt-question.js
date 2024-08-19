9430c7a8406c05b2888fedd10b2436558ed77a09
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 00:14:09 -0600

    added question to Egypt page

---
+++ egypt-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("Is the importance of camels in Egypt, an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Determinism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
