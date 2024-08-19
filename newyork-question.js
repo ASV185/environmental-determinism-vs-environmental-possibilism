d28d92512b5f81e01514efda483837374031aeea
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 00:55:20 -0600

    added question to New York page

---
+++ newyork-question.js
@@ -0,0 +1,7 @@
+let environmentalReaction = prompt("New York's billion dollar sea wall, an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
+
+if (environmentalReaction === "Environmental Possibilism") {
+  alert("Yes, congratulations!");
+} else {
+  alert("No, you should try it again.");
+}
