d03d97eac4acb7f1393a11cb1b8158151174c330
Amanda Ventura <saenz242ever@gmail.com>
2024-08-19 23:38:12 -0600

    added more HTML content and changed JavaScript to Japan pages

---
+++ japan-question.js
@@ -0,0 +1,14 @@
+function getSelectedAnswer(event){
+  if (event.target.value.length > 0) {
+    if (event.target.value === "determinism") {
+      alert("Yes, congratulations!");
+    }
+    if (event.target.value === "possibilism") {
+      alert("No, you should try again.");
+    }
+  }
+}
+
+let form = document.querySelector("#question-form");
+form.addEventListener("change", getSelectedAnswer);
+
--- questions.js
+++
@@ -1,9 +0,0 @@
-let environmentalReaction = prompt("Is Japanese Seafood an example of Environmental Determinism or Environmental Possibilism? (Capitalize first letter of each word)" );
-
-if (environmentalReaction === "Environmental Determinism") {
-  alert("Yes, congratulations!");
-} else {
-  alert("No, you should try it again.");
-}
-
-
